30a477db386bcd6c37836061055bee76981876e9
npomfret <nick-github@snowmonkey.co.uk>
2026-04-10 13:06:50 +0100

    no-spoilers 1.0.19

--- Casks/no-spoilers.rb
+++ Casks/no-spoilers.rb
@@ -1,6 +1,6 @@
 cask "no-spoilers" do
-  version "1.0.18"
-  sha256 "f9e67655173d4f79349d8f6283114f3ea696f046bcdd507364487daba996eb82"
+  version "1.0.19"
+  sha256 "09ceaa79517729cd2be4a5f0864ea31afae35394f25798ada28772d2839a26d6"
 
   url "https://github.com/npomfret/no-spoilers/releases/download/v#{version}/NoSpoilers-#{version}.zip"
   name "No Spoilers"
